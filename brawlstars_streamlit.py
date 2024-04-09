3572a52188ab2eacc8e4e659b1d8d4f5ff017bff
pablo-csv <83393209+pablo-csv@users.noreply.github.com>
2024-04-09 16:50:02 +0200

    Update brawlstars_streamlit.py

--- brawlstars_streamlit.py
+++ brawlstars_streamlit.py
@@ -1,6 +1,6 @@
 
 import requests
-#import streamlit as st
+import streamlit as st
 
 def get_battlelog(player_tag, my_key):
     api_url = f'https://api.brawlstars.com/v1/players/%23{player_tag[1:]}/battlelog'
@@ -22,12 +22,12 @@ def get_battlelog(player_tag, my_key):
 my_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImY3ZDcyYzA0LTYyY2YtNGU5Yy1iMDE5LTA4Y2IzYzg5ZWYwMSIsImlhdCI6MTcxMjY3Mjc0Miwic3ViIjoiZGV2ZWxvcGVyL2MxNGEwOGRiLTZiMmQtYTc5OC02YTc2LWJlZDcwN2JjNzAxMiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTU4LjQyLjE3Mi4yOCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.e2DU_v7pfH5nRIEC1Sh5J95oQ1AKvqx1wJegsddGdOpA9eJN2ce1hp3q6S2T1-NNUW_T4u1-pBLstGg8elMsyg'
 
 
-'''
+
 # CONFIGURACIÓN DE LA WEB
 st.set_page_config(layout="wide", page_title="Liga Terraplanista")
 
 player_name = st.selectbox('Jugador', pred_players.keys(), index=1)
-'''
+
 
 pred_players = {'Matías': '#YGG02LP0',
                 'Merto': '#298QCP0L8',
